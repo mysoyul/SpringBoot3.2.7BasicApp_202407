07ea139fc045a2de749f28298ed14b052392fe90
mysoyul <vega2k_@naver.com>
2024-07-04 17:40:09 +0900

    userinfo add

--- src/main/java/com/basic/myspringboot/security/config/SecurityConfig.java
+++ src/main/java/com/basic/myspringboot/security/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.basic.myspringboot.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,6 +24,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> {
+                    //auth.requestMatchers(HttpMethod.POST,"/api/users/**").permitAll();
                     auth.requestMatchers("/api/users/welcome").permitAll()
                             .requestMatchers("/api/users/**").authenticated();
                 })
