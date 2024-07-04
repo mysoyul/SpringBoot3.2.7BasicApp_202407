1ee2faf3aabe93e0ac1dd7366c74f8cf59acac17
mysoyul <vega2k_@naver.com>
2024-07-04 10:40:53 +0900

    CustomerRepositoryTest update

--- src/test/java/com/basic/myspringboot/model/CustomerRepositoryTest.java
+++ src/test/java/com/basic/myspringboot/model/CustomerRepositoryTest.java
@@ -3,6 +3,8 @@ package com.basic.myspringboot.model;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -10,12 +12,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class CustomerRepositoryTest {
 
     @Autowired
     CustomerRepository customerRepository;
 
     @Test
+    @Rollback(value = false)
     public void customer() throws Exception {
         //등록
         Customer customer = new Customer();
@@ -25,6 +29,7 @@ class CustomerRepositoryTest {
         System.out.println(addCustomer.getCustomerId() + " " + addCustomer.getCustomerName());
         assertThat(addCustomer).isNotNull();
         assertThat(addCustomer.getCustomerName()).isEqualTo("스프링");
+        assertEquals("A001", addCustomer.getCustomerId());
 
         Optional<Customer> existCustomer = customerRepository.findByCustomerId(addCustomer.getCustomerId());
         assertThat(existCustomer).isNotEmpty();
