c6086e135500cc1b7c2ff7f2c47528d2ab44489a
mysoyul <vega2k_@naver.com>
2024-07-04 13:12:40 +0900

    UserRestController getUser() update

--- src/main/java/com/basic/myspringboot/controller/UserRestController.java
+++ src/main/java/com/basic/myspringboot/controller/UserRestController.java
@@ -1,11 +1,15 @@
 package com.basic.myspringboot.controller;
 
+import com.basic.myspringboot.exception.BusinessException;
 import com.basic.myspringboot.model.user.User;
 import com.basic.myspringboot.model.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,4 +31,25 @@ public class UserRestController {
     public List<User> getUsers() {
         return userRepository.findAll();
     }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userRepository.findById(id) //Optional<User>
+                .orElseThrow(() -> new BusinessException("User Not Found", HttpStatus.NOT_FOUND));
+    }
+//    public User getUser(@PathVariable Long id) {
+//        Optional<User> optionalUser = userRepository.findById(id);
+//        //orElseThrow(Supplier) Supplier 추상메서드 T get()
+//        //1. Anonymous Inner Class
+//        User existUser = optionalUser.orElseThrow(new Supplier<BusinessException>() {
+//            @Override
+//            public BusinessException get() {
+//                return new BusinessException("User Not Found", HttpStatus.NOT_FOUND);
+//            }
+//        });
+//        return existUser;
+//    }
+
+
+
 }
