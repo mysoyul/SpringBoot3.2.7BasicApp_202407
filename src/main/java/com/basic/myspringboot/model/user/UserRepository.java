0e1d7a794e6c5b737ba87f750e74e16a9bdb6a0e
mysoyul <vega2k_@naver.com>
2024-07-04 11:07:18 +0900

    UserRepository add

---
+++ src/main/java/com/basic/myspringboot/model/user/UserRepository.java
@@ -0,0 +1,11 @@
+package com.basic.myspringboot.model.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByName(String name);
+    Optional<User> findByEmail(String email);
+}
