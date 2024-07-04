f747265f0d3f345a7fbd5c6e59fcce1cb69d7721
mysoyul <vega2k_@naver.com>
2024-07-04 11:31:58 +0900

    UserRestController getUser() add

--- src/main/java/com/basic/myspringboot/controller/UserRestController.java
+++ src/main/java/com/basic/myspringboot/controller/UserRestController.java
@@ -3,10 +3,9 @@ package com.basic.myspringboot.controller;
 import com.basic.myspringboot.model.user.User;
 import com.basic.myspringboot.model.user.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,6 +22,12 @@ public class UserRestController {
     public User create(@RequestBody User user) {
         return userRepository.save(user);
     }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
 
 
 }
